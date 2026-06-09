ce286a263397e41f080d8e77dc611a056a3a8875
diegol447 <diegoamoreira33@gmail.com>
2026-06-09 12:30:00 +0000

    criacao e edicao de anfitrioes

---
+++ src/views/anfitrioes/CriarAnfitriaoView.vue
@@ -0,0 +1,28 @@
+﻿<template>
+  <TelaFormulario
+    titulo="Anfitriao"
+    endpoint="/anfitrioes/"
+    :campos="campos"
+  />
+</template>
+
+<script>
+import TelaFormulario from '@/components/TelaFormulario.vue'
+
+export default {
+  name: 'CriarAnfitriaoView',
+  components: { TelaFormulario },
+  data() {
+    return {
+      campos: [
+        { nome: 'nome', label: 'Nome' },
+        { nome: 'email', label: 'E-mail', tipo: 'email' },
+        { nome: 'telefone', label: 'Telefone' },
+        { nome: 'documento', label: 'Documento' },
+        { nome: 'bio', label: 'Biografia', multiLinha: true },
+        { nome: 'avaliacao_media', label: 'Avaliacao media', tipo: 'number', valorPadrao: '0.00' },
+      ],
+    }
+  },
+}
+</script>
---
+++ src/views/anfitrioes/EditarAnfitriaoView.vue
@@ -0,0 +1,28 @@
+﻿<template>
+  <TelaFormulario
+    titulo="Editar Anfitriao"
+    endpoint="/anfitrioes/"
+    :campos="campos"
+  />
+</template>
+
+<script>
+import TelaFormulario from '@/components/TelaFormulario.vue'
+
+export default {
+  name: 'EditarAnfitriaoView',
+  components: { TelaFormulario },
+  data() {
+    return {
+      campos: [
+        { nome: 'nome', label: 'Nome' },
+        { nome: 'email', label: 'E-mail', tipo: 'email' },
+        { nome: 'telefone', label: 'Telefone' },
+        { nome: 'documento', label: 'Documento' },
+        { nome: 'bio', label: 'Biografia', multiLinha: true },
+        { nome: 'avaliacao_media', label: 'Avaliacao media', tipo: 'number' },
+      ],
+    }
+  },
+}
+</script>
