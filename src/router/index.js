2ebe3527a9865b7f676eec53ca8c673a70922fa3
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-06-10 17:00:00 +0000

    adiciona guards de autenticacao nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -66,4 +66,9 @@ const routes = [
   { path: '/comodidades/:id/editar', name: 'EditarComodidade', component: EditarComodidadeView },
 ]
 const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
+router.beforeEach((to, from) => {
+  if (to.name !== 'login' && !localStorage.getItem('token')) return { name: 'login' }
+  return true
+})
+
 export default router
