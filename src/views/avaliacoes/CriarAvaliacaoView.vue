955c7a61b2187a73d0a2614b7fcdc51ca0785386
augusto-dmh <augustodemelohenriques@gmail.com>
2026-06-10 13:15:00 +0000

    criacao e edicao de avaliacoes

---
+++ src/views/avaliacoes/CriarAvaliacaoView.vue
@@ -0,0 +1,27 @@
+﻿<template>
+  <TelaFormulario
+    titulo="Avaliacao"
+    endpoint="/avaliacoes/"
+    :campos="campos"
+  />
+</template>
+
+<script>
+import TelaFormulario from '@/components/TelaFormulario.vue'
+
+export default {
+  name: 'CriarAvaliacaoView',
+  components: { TelaFormulario },
+  data() {
+    return {
+      campos: [
+        { nome: 'hospedagem', label: 'ID da Hospedagem', numero: true },
+        { nome: 'nome_hospede', label: 'Nome do Hospede' },
+        { nome: 'email', label: 'E-mail', tipo: 'email' },
+        { nome: 'nota', label: 'Nota', numero: true },
+        { nome: 'comentario', label: 'Comentario', multiLinha: true },
+      ],
+    }
+  },
+}
+</script>
